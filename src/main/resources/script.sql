65a8c5585f2f40f9f13854a52f153218bd8fcdd7
Fabricio Caires <fabricio.c.vieira@ibm.com>
2024-02-07 18:33:33 -0300

    configs nos arquivos sql e nginx.conf

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -7,8 +7,19 @@ CREATE TABLE CLIENTE (
 CREATE TABLE TRANSACAO (
     id serial PRIMARY KEY,
     client_id integer REFERENCES CLIENTE (id),
-    realizado_em TIMESTAMP NOT NULL DEFAULT now(),
+    realizado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
     tipo char(1) not null,
     descricao varchar(10) not null,
     valor integer not null
 );
+
+
+DO $$
+BEGIN
+    INSERT INTO CLIENTE (limite, saldo)
+        VALUES (100000, 0),
+        VALUES (80000, 0),
+        VALUES (1000000, 0),
+        VALUES (10000000, 0),
+        VALUES (500000, 0);
+END; $$
