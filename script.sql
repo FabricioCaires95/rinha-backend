1ca54c5fd09d38553b6b3665815c52a6a3bbdfa1
Fabricio Vieira <fabricio.vieira995@hotmail.com>
2024-03-09 00:39:28 -0300

    criação de indexes e configs gatling/docker compose

--- script.sql
+++ script.sql
@@ -1,10 +1,10 @@
-CREATE TABLE IF NOT EXISTS public.CLIENTE (
+CREATE UNLOGGED TABLE IF NOT EXISTS public.CLIENTE (
     id serial PRIMARY KEY,
     limite integer not null,
     saldo  integer not null
 );
 
-CREATE TABLE IF NOT EXISTS public.TRANSACAO (
+CREATE UNLOGGED TABLE IF NOT EXISTS public.TRANSACAO (
     id serial PRIMARY KEY,
     client_id integer REFERENCES CLIENTE (id),
     realizado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -13,6 +13,8 @@ CREATE TABLE IF NOT EXISTS public.TRANSACAO (
     valor integer not null
 );
 
+CREATE INDEX idx_transacao_cliente_id ON transacao (client_id);
+CREATE INDEX idx_transacao_id_cliente_realizado_em ON transacao (client_id, realizado_em DESC);
 
 DO $$
 BEGIN
